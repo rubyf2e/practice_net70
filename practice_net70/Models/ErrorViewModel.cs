917ef638162cea69300fbfd45af7bafa38a48679
ruby <rubyf2e@gmail.com>
2023-03-23 00:15:38 +0800

    init

---
+++ practice_net70/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace practice_net70.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
