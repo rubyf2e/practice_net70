7d777b3ced9408a3b936b37725a16e4f8e60fc9b
ruby <rubyf2e@gmail.com>
2023-03-23 10:50:28 +0800

    首頁放電影列表

--- practice_net70/Controllers/HomeController.cs
+++ practice_net70/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using practice_net70.Data;
 using practice_net70.Models;
 
 namespace practice_net70.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ApplicationDbContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        return _context.Movie != null ?
+                 View(await _context.Movie.ToListAsync()) :
+                 Problem("Entity set 'ApplicationDbContext.Movie'  is null.");
     }
 
     public IActionResult Privacy()
