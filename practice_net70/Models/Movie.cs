f8eba6ac040cfd94f6ec3882671fcb89194d48ed
ruby <rubyf2e@gmail.com>
2023-03-23 11:00:19 +0800

    改 Movie model 裡的標題

--- practice_net70/Models/Movie.cs
+++ practice_net70/Models/Movie.cs
@@ -6,10 +6,18 @@ namespace practice_net70.Models
 	public class Movie
 	{
         public int Id { get; set; }
+
+        [Display(Name = "名稱")]
         public string? Title { get; set; }
+
+        [Display(Name = "上映時間")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "類別")]
         public string? Genre { get; set; }
+
+        [Display(Name = "價錢")]
         public decimal Price { get; set; }
     }
 }
